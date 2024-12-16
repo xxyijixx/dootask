e4faf0bc8062b6273a3ad9e07da2c0b34a98608b
lt54268 <1060075426@qq.com>
2024-12-16 18:46:42 +0800

    feat: 新增云图标
    (cherry picked from commit 6be8b3798e8b7de7f331e508af12a175493a163c)

--- resources/assets/js/store/mutations.js
+++ resources/assets/js/store/mutations.js
@@ -1,3 +1,10 @@
 export default {
-
+    /**
+     * 设置云存储状态
+     * @param state
+     * @param data {id, status}
+     */
+    SET_CLOUD_STORAGE(state, {id, status}) {
+        state.cacheCloudStorage[id] = status;
+    }
 }
